0f55176d7a8fc93dbdd21e18cb2a13d5d887adc5
karity-0 <260622833+karity-0@users.noreply.github.com>
2026-05-01 17:45:13 +0900

    feat: implement string obfuscation

--- obfuscator.py
+++ obfuscator.py
@@ -2,6 +2,11 @@ import sys
 import argparse
 from pathlib import Path
 
+from luaparser import ast, astnodes
+
+def encode(s):
+    return '"' + ''.join(f'\\{ord(c)}' for c in s) + '"'
+
 class Obfuscator:
     def __init__(self):
         self.args = None
@@ -14,7 +19,14 @@ class Obfuscator:
 
         input_script    = self.read_input()
         output_path     = self.get_output_path()
-        output_script   = self.obfuscate(input_script)
+        
+        print("parsing script..")
+        tree = ast.parse(input_script)
+
+        print("obfuscating..")
+        output_script   = self.obfuscate(input_script, tree)
+
+        print("saving output..")
         self.write_output(output_path, output_script)
         sys.exit(0)
 
@@ -42,10 +54,30 @@ class Obfuscator:
             sys.exit(1)
 
 
-    def obfuscate(self, script: str) -> str:
-        output = f"-- obfuscated!\n{script}"
-        return output
+    def obfuscate(self, script: str, tree) -> str:
+        replacements = []
+    
+        for node in ast.walk(tree):
+            if isinstance(node, astnodes.String):
+                encoded = encode(node.raw)
+                #print(ast.to_pretty_str(node))
+                print(f"[String] {node.s} -> {encoded}")
+                print(node.start_char, node.stop_char)
+                print(node.first_token, node.last_token)
+                replacements.append((
+                    node.start_char,
+                    node.stop_char,
+                    encoded
+                ))
         
+        return self.apply(script, replacements)
+        
+    def apply(self, src, replacements):
+        for start, end, new in sorted(replacements, reverse=True):
+            src = src[:start] + new + src[end+1:]
+
+        src = f"-- obfuscated!\n{src}"
+        return src
 
 if __name__ == "__main__":
     obfuscator = Obfuscator()
