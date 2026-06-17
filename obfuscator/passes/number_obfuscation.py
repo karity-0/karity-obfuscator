ab2ea24a60469e50fecef0e8e17c532a4d4692bd
karity-0 <260622833+karity-0@users.noreply.github.com>
2026-06-17 12:51:14 +0900

    feat(number_obf): increase obfuscation depth back to 3

--- obfuscator/passes/number_obfuscation.py
+++ obfuscator/passes/number_obfuscation.py
@@ -104,7 +104,7 @@ class NumberObfuscationPass(BasePass):
             else:
                 expr = self._gen_int_expr(
                     _parse_int_token(token),
-                    random.randint(1, 1),
+                    random.randint(1, 3),
                 )
 
             replacements.append(
